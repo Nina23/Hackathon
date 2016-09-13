25b141e0286e70e164d080e1ec400a642dc7ef76
vagrant <vagrant@precise32.(none)>
2016-09-13 10:57:31 +0000

    Mau

--- app/Location.php
+++ app/Location.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     protected $table = 'location';
-    protected $fillable=['child','lang','lat','time_of_location','speed','state','accurancy','provider'];
+    protected $fillable=['child','lang','lat','time_of_location','speed','state','accuracy','provider'];
 }
